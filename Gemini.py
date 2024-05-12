692368405fb13da6987f8dc9be7a63f341ecc717
Songporworn Mahapiyanont <167118862+SongporwornMahapiyanont@users.noreply.github.com>
2024-05-13 05:55:05 +0700

    Update Gemini.py

--- Gemini.py
+++ Gemini.py
@@ -3,9 +3,7 @@ import io
 from PIL import Image
 import google.generativeai as genai
 
-import GT
-import cv2
-from stability_ai import text2image
+
 
 genai.configure(api_key="AIzaSyA0TwQnk0mKo9rgnty9z9l05hoz7F2dfmU")
 model = genai.GenerativeModel("gemini-pro-vision")
@@ -60,31 +58,7 @@ if st.button("Translate"):
 
 
 
-api_key   = "AIzaSyA0TwQnk0mKo9rgnty9z9l05hoz7F2dfmU"
-engine_id = "stable-diffusion-v1-6"
-filename_save = "image_out.jpg"
 
-st.title("สร้างภาพจากข้อความ")
-prompt = st.text_input("ป้อน prompt ภาษาไทย: ")
-
-ch = st.selectbox("เลือกรูปแบบ",
-                 ("watercolor painting",
-                  "cartoon line drawing",
-                  "flat cartoon illustration",
-                  "sticker",
-                  "3d rendering",
-                  "kid crayon drawing"))
-
-if st.button("สร้างภาพ"):      
-    try:
-        prompt_en = GT.translate(prompt,'th','en') # th --> en
-        prompt = prompt_en + " , " + ch
-        st.text(prompt)
-        text2image(api_key,engine_id,prompt,filename_save)
-        img = cv2.imread(filename_save)
-        st.image(img,channels="BGR")
-    except:
-        st.text("ลองใหม่อีกครั้ง")
 
 
 
