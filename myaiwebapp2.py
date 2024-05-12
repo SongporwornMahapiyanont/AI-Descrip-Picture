56d87be96731b1d810825617a59cad170e4c6186
Songporworn Mahapiyanont <167118862+SongporwornMahapiyanont@users.noreply.github.com>
2024-05-13 05:27:21 +0700

    Update myaiwebapp2.py

--- myaiwebapp2.py
+++ myaiwebapp2.py
@@ -8,7 +8,7 @@ model = genai.GenerativeModel("gemini-pro-vision")
 
 st.set_page_config(page_title="LNC-BOT")
 st.title("Welcome To LNC-BOT")
-st.markdown(f'<p style="background-color:#C2787A;color:#ffffff;font-size:150%;text-align:center">{"Create by Songporworn Mahapiyanont"}</p>', unsafe_allow_html=True)
+st.markdown(f'<p style="background-color:#1C3578;color:#ffffff;font-size:150%;text-align:center">{"Create by Songporworn Mahapiyanont"}</p>', unsafe_allow_html=True)
 text4 = "AI By Gemini (google)"
 st.markdown(f'<p style="text-align:right">{text4}</p>', unsafe_allow_html=True)
 st.text("This Web is My project for reseach, learning ,etc. You can copy for using learning etc. Have for fun Guy!")
