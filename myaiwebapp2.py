be6884a0701a72cd58b0af080d7cc3d37a0f25ad
Songporworn Mahapiyanont <songporwornmahapiyanont@gmail.com>
2024-05-13 03:19:26 +0700

    Add code Test Gemini

---
+++ myaiwebapp2.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import io
+from PIL import Image
+
+import google.generativeai as genai
+genai.configure(api_key="AIzaSyA0TwQnk0mKo9rgnty9z9l05hoz7F2dfmU")
+model = genai.GenerativeModel("gemini-pro-vision")
+
+st.title("ยินดีต้อนรับสู่การบรรยายภาพของบอท")
+#ch = st.selectbox("เลือกหมวด",
+#                ("สัตว์","สิ่งของ","ผลไม้","ดอกไม้"))
+                  
+prompt = ch + "บรรยายภาพนี้อย่างละเอียด"
+
+img_file = st.file_uploader("เปิดไฟล์ภาพ")
+
+if img_file is not None:
+    imagefile = io.BytesIO(img_file.read())
+    img = Image.open(imagefile)
+    st.image(img_file,channels="BGR")
+
+if st.button("ประมวลผล"):
+    try:
+        response = model.generate_content([img,prompt])
+        st.text(response.text)
+    except:
+        st.text("no response")
+
+
+
+
+    
+
+
+
