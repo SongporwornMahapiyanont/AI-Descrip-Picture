d2cb631cc7c90a366e209bb043bd342f004fd187
Songporworn Mahapiyanont <167118862+SongporwornMahapiyanont@users.noreply.github.com>
2024-05-13 14:15:15 +0700

    Update Gemini.py

--- Gemini.py
+++ Gemini.py
@@ -10,7 +10,7 @@ model = genai.GenerativeModel("gemini-pro-vision")
 
 st.set_page_config(page_title="LNC-BOT")
 st.title("Welcome To LNC-BOT")
-st.markdown(f'<p style="background-color:#cc6666;color:#ffffff;font-size:150%;text-align:center">{"Create by Songporworn Mahapiyanont"}</p>', unsafe_allow_html=True)
+st.markdown(f'<p style="background-color:#cc6666;color:#ffffff;font-size:150%;text-align:center">{"Created by Songporworn Mahapiyanont"}</p>', unsafe_allow_html=True)
 text4 = "AI By Gemini (google)"
 st.markdown(f'<p style="text-align:right">{text4}</p>', unsafe_allow_html=True)
 st.markdown(f'<p style="text-align:center">{"This Web is My project for reseach, learning ,etc."}</p>', unsafe_allow_html=True)
@@ -22,14 +22,14 @@ st.write(a)
                   
 prompt = "คืออะไรไม่ต้องบอกระเอียดมาก ถ้าสิ่งของมีสีดำหรือเทาหรือน้ำตาลให้บอกว่า'เสีย'และถ้าไม่มีให้บอกว่า'ไม่เสีย' มีจำนวนเท่าไหร่บอกหน่วยด้วยและมีประมาณกี่กิโลกรัมทศนิยมสองตำแหน่งและแช่ในตู้เย็นจะหมดอายุประมาณกี่วัน ตอบทุกคำถามจัดระเบียบคำตอบให้สวยงาม"
 
-img_file = st.file_uploader("เปิดไฟล์ภาพ")
+img_file = st.file_uploader("openfile")
 
 if img_file is not None:
     imagefile = io.BytesIO(img_file.read())
     img = Image.open(imagefile)
     st.image(img_file,channels="BGR")
 
-if st.button("ประมวลผล"):
+if st.button("Process"):
     try:
         response = model.generate_content([img,prompt])
         st.text(response.text)
